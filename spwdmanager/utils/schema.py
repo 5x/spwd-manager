88427fcf59de9852f24707194273b3dff0104005
Serhii Zarutskyi <5x@users.noreply.github.com>
2023-03-24 04:15:17 +0200

    Update Declarative object validator

--- spwdmanager/utils/schema.py
+++ spwdmanager/utils/schema.py
@@ -8,27 +8,31 @@ def validate(value, schema):
         except (TypeError, ValueError):
             return 0
     elif schema is str:
-        if isinstance(type(value), type):
+        if isinstance(value, type):
             return ""
         return str(value).strip()
     elif schema is bool:
-        if isinstance(type(value), type):
+        if isinstance(value, type):
             return False
         return bool(value)
     elif isinstance(schema, (tuple, list, dict)):
-        if value is None or not isinstance(value, type(schema)):
+        if not value or not isinstance(value, type(schema)):
             return type(schema)()
         if not schema:
             return value
         if isinstance(schema, dict):
             collection = {}
             for k in schema.keys():
-                node = value if k in value else schema
-                collection[k] = validate(node[k], schema[k])
+                node_value = value if k in value else schema
+                node = validate(node_value[k], schema[k])
+                if node:
+                    collection[k] = node
             return collection
         else:
             collection = []
             for item in value:
-                collection.append(validate(item, schema[0]))
+                node = validate(item, schema[0])
+                if node:
+                    collection.append(validate(item, schema[0]))
             return type(schema)(collection)
     return None
